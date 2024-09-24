a1bef39dfe737f007968fda79e15d1f60235fe73
Van Dong <thenullmastermind@gmail.com>
2024-09-24 16:25:01 +0700

    chore: add uuid dep and create config gen funcs
    - Add uuid package for generating unique names
    - Create funcs to generate frp configs for port and domain

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const { v4 } = require("uuid");
+
+const generateConfigByPort = ({ localPort, remotePort, name }) => {
+  name = name || v4();
+  localPort = localPort || remotePort;
+  remotePort = remotePort || localPort;
+
+  return `
+serverAddr = "${process.env.REMOTE_HOST}"
+serverPort = 7000
+
+[[proxies]]
+name = "${name}"
+type = "tcp"
+localIP = "127.0.0.1"
+localPort = ${localPort}
+remotePort = ${remotePort}
+  `;
+};
+
+const generateConfigByDomain = ({ localPort, name, customDomain }) => {
+  name = name || v4();
+  localPort = localPort || 7000;
+  customDomain = customDomain || `${name}.dongnv.dev`;
+
+  return `
+serverAddr = "${process.env.REMOTE_HOST}"
+serverPort = 7000
+
+[[proxies]]
+name = "${name}"
+type = "http"
+localPort = ${localPort}
+customDomains = ["${customDomain}"]
+  `;
+};
+
 console.log(process.env.REMOTE_HOST);
